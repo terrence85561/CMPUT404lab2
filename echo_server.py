b6bea1656b4a2066c6abdcb21e0b8919ea002dfd
Tianyi  Liang <terrence8556@gmail.com>
2019-01-16 17:42:24 -0700

    echo_server.py

---
+++ echo_server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import socket
+
+# it means listening to local host
+HOST = ""
+PORT = 8001
+Buffer_SIZE = 1024
+
+def main():
+    #create socket
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        s.bind((HOST,PORT))
+        s.listen(1) # make socket listen
+
+
+        #listen forever for connections
+        while True:
+            conn,addr = s.accept() # accept any incoming connections and store the addr connects with me
+            full_data = b""
+
+            while True:
+                data = conn.recv(Buffer_SIZE)
+                if not data:
+                    break
+                full_data += data
+                #print(full_data)
+                #send data back as response
+                conn.sendall(full_data)
+if __name__ == "__main__":
+    main()
